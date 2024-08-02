e2423c6fe70f09da05b171f6b9e38289d14bbbed
Bryant Santiago <pazbryant@proton.me>
2024-08-02 16:31:20 -0600

    chore: add pmd configuration

--- home.nix
+++ home.nix
@@ -180,6 +180,18 @@
     network = {
      startWhenNeeded = true;
     };
+    extraConfig = ''
+     audio_output {
+      type "pipewire"
+      name "PipeWire Sound Server"
+     }
+     audio_output {
+      type "fifo"
+      name "my_fifo"
+      path "/tmp/mpd.fifo"
+      format "44100:16:2"
+     }
+    '';
    };
 
    dunst = {
